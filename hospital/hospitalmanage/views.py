1ca4c91449227b91d5555bb7997639053b9d9ba0
AryaAravind <aryasuku4823@gmail.com>
2023-01-06 10:56:50 +0530

    first commit

---
+++ hospital/hospitalmanage/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from . models import Department,Doctors
+from.forms import BookingForm
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def contact(request):
+    return render(request,'contact.html')
+def department(request):
+    dict_dept={
+        'dept':Department.objects.all()
+    }
+    return render(request,'department.html',dict_dept)
+def about(request):
+    return render(request,'about.html')
+def booking(request):
+    if request.method=="POST":
+        form=BookingForm(request.POST)
+        if form.is_valid():
+            form.save() 
+            return  render(request,'conformation.html')
+    formvb= BookingForm()
+    dict_book={
+        'book':formvb
+    }
+
+    return render(request,'booking.html',dict_book)
+def doctors(request):
+    dict_doc={
+        'doc':Doctors.objects.all()
+    }
+    return render(request,'doctors.html',dict_doc)
